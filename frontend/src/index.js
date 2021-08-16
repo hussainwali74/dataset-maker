0b499becf9df88cb880e59cf015016d844c15dc8
unknown <13bscshwali@seecs.edu.pk>
2021-08-16 22:52:46 +0500

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 import App from "./App"
+import axios from "axios"
+import { getToken, TOKEN } from "./helpers/auth"
+
+// ========================================================================
+// =========================   AXIOS configurations   =====================
+// ========================================================================
+// server
+axios.defaults.baseURL = "http://roomie.pk:5000/"
+// local
+// axios.defaults.baseURL = 'http://localhost:5000/loadex/'
+
+axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem(TOKEN)
+axios.defaults.headers.post["Content-Type"] = "application/json"
+
+axios.interceptors.request.use(
+	(request) => {
+		// console.log('REQUEST INTERCEPTED: ', request)
+		// Edit request config
+		request.headers = { Authorization: " Bearer " + getToken() }
+		return request
+	},
+	(error) => {
+		// console.log(error)
+		return Promise.reject(error)
+	}
+)
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
 )
- 
